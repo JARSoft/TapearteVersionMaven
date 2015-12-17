b57a97db1b680d2555327f77798b945787ae1cd3
Jhuanaldo <jagg.-q@hotmail.com>
2015-12-17 23:23:15 +0100

    Octava Iteración con Maven

---
+++ my-android-application/src/main/java/your/company/Dominio/Cliente/GestorComentar.java
@@ -0,0 +1,15 @@
+package your.company.Dominio.Cliente;
+
+public class GestorComentar {
+
+	/**
+	 * 
+	 * @param t
+	 * @param comentario
+	 */
+	public String comentar(Tapa t, String comentario) {
+		// TODO - implement GestorComentar.comentar
+		throw new UnsupportedOperationException();
+	}
+
+}
